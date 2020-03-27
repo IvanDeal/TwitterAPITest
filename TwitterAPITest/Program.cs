9017b221b475092d4d22d3e1e81cc5d025baf965
Ivan <46203487+IvanDeal@users.noreply.github.com>
2020-03-27 13:57:29 -0700

    Initial Commit

---
+++ TwitterAPITest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using TweetSharp;
+
+namespace TwitterAPITest
+{
+    class Program
+    {
+        private static string customer_key = "E0PEEBZ5Lc9I7UUJYrjCpJREY";
+        private static string customer_key_secret = "gzoElfbzkWqbzIgwASF7CNpVDlOdAhPpYbFpcXdAqPXVPHUSiS";
+        private static string access_token = "21425165-dZfigXCYZNz0pHaPD6MQgA407fqb7pYGpHZb8xBeF";
+        private static string access_token_secret = "TNrbCkQ7rPzM3uBjrPVFWrjt8WRhRl9kNSfzYrsdwJ8gL";
+
+        private static TwitterService service = new TwitterService(customer_key, customer_key_secret, access_token, access_token_secret);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"<{DateTime.Now}>");
+
+        }
+
+        private static void SendTweet(string _status)
+        {
+
+        }
+    }
+}
