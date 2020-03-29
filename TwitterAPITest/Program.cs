eb0d300b1d4e16aafb4dd234e8e53be726f66372
Ivan <46203487+IvanDeal@users.noreply.github.com>
2020-03-29 12:34:15 -0700

    Initial Commit

--- TwitterAPITest/Program.cs
+++ TwitterAPITest/Program.cs
@@ -4,27 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Net;
 using TweetSharp;
 
 namespace TwitterAPITest
 {
     class Program
     {
-        private static string customer_key = "E0PEEBZ5Lc9I7UUJYrjCpJREY";
-        private static string customer_key_secret = "gzoElfbzkWqbzIgwASF7CNpVDlOdAhPpYbFpcXdAqPXVPHUSiS";
-        private static string access_token = "21425165-dZfigXCYZNz0pHaPD6MQgA407fqb7pYGpHZb8xBeF";
-        private static string access_token_secret = "TNrbCkQ7rPzM3uBjrPVFWrjt8WRhRl9kNSfzYrsdwJ8gL";
+        private static string customer_key = "###############################";
+        private static string customer_key_secret = "###################################";
+        private static string access_token = "##########################################";
+        private static string access_token_secret = "#############################################";
 
         private static TwitterService service = new TwitterService(customer_key, customer_key_secret, access_token, access_token_secret);
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"<{DateTime.Now}>");
-
+            Console.WriteLine($"<{DateTime.Now}> - What would you like to Tweet?");
+            string userInput = Console.ReadLine();
+            SendTweet(userInput);
+            Console.Read();
         }
 
         private static void SendTweet(string _status)
         {
+            service.SendTweet(new SendTweetOptions { Status = _status }, (tweet, response) =>
+                 {
+                     if (response.StatusCode == HttpStatusCode.OK)
+                     {
+                         Console.ForegroundColor = ConsoleColor.Green;
+                         Console.WriteLine($"<{DateTime.Now}> - Tweet Sent!");
+                         Console.ResetColor();
+                     }
+                     else
+                     {
+                         Console.ForegroundColor = ConsoleColor.Red;
+                         Console.WriteLine($"<ERROR> " + response.Error.Message);
+                         Console.ResetColor();
+                     }
+                 });
 
         }
     }
